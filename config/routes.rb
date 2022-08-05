819e66b0bd93aa105b08d67b131a19d2226f3d41
EC2 Default User <ec2-user@ip-172-31-35-141.ap-northeast-1.compute.internal>
2022-08-05 16:21:06 +0000

    13章まで。１０章でエラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :index, :show]
+                          #onlyはその中のアクションしかルーティングしない。
   devise_for :users
   root to: 'homes#top'
   get '/homes/about' => 'homes#about'
  
+  resources :post_images, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
